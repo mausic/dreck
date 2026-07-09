91595c8e71d75f7caf1eb4fe80f776ccf982bb37
Maksym Ryndia <ryndya.maxim@gmail.com>
2026-07-09 13:24:19 +0300

    chore: add prettier, format existing codebase

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  arrowParens: "always",
+  tabWidth: 2,
+  objectWrap: "preserve",
+};
+
+export default config;
