f579ca4effe4cd9088893743f81bf53e37072c12
Maksym Ryndia <ryndya.maxim@gmail.com>
2026-07-09 19:50:34 +0300

    style(web): config shadcn

--- eslint.config.ts
+++ eslint.config.ts
@@ -38,6 +38,7 @@ export default defineConfig([
     ignores: [
       "prettier.config.js",
       "commitlint.config.js",
+      "src/components/ui/**/*.tsx",
       "src/routeTree.gen.ts",
       ".tanstack",
       ".vscode",
