ac4a0d39c1ccd3cc5c37ece0b1b6782c24d65989
Maksym Ryndia <ryndya.maxim@gmail.com>
2026-07-17 12:54:47 +0300

    chore(web): update web page title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -27,7 +27,7 @@ export const Route = createRootRouteWithContext<{
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "Dreck | AI slide generator",
       },
     ],
     links: [
